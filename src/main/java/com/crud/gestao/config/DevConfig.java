ee37e9ddd3e3f419614e8fd4f9ef8505cd4f35d9
leonardols10 <leonardosilvalima40@gmail.com>
2022-09-25 17:06:23 -0300

    perfil dev

---
+++ src/main/java/com/crud/gestao/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.crud.gestao.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.crud.gestao.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	
+	@Bean
+	public boolean  instanciaDB() {
+		if(value.equals("create")) {
+			this.dbservice.instanciaDB();
+		}
+		return false;
+	}
+
+}
