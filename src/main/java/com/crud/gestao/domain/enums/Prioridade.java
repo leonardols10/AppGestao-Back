3d6c6340edf9f59372a34c4509943e7981595ed3
leonardols10 <leonardosilvalima40@gmail.com>
2022-09-24 22:02:25 -0300

    status e prioridade

--- src/main/java/com/crud/gestao/domain/enums/Prioridade.java
+++ src/main/java/com/crud/gestao/domain/enums/Prioridade.java
@@ -2,7 +2,7 @@ package com.crud.gestao.domain.enums;
 
 public enum Prioridade {
 	
-	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
 
 	private Integer codigo;
 	private String descricao;
@@ -32,7 +32,7 @@ public enum Prioridade {
 			}
 		}
 		
-		throw new IllegalArgumentException("Status inválido");
+		throw new IllegalArgumentException("Prioridade inválido");
 	}
 	
 	
