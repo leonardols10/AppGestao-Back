ea26d3b818f0254373b73a56972db5fe1c44e6ca
leonardols10 <leonardosilvalima40@gmail.com>
2022-09-25 21:13:21 -0300

    validação de cpf e email já cadastrados

--- src/main/java/com/crud/gestao/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/crud/gestao/resources/exceptions/ResourceExceptionHandler.java
@@ -7,10 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.crud.gestao.services.exceptions.DataIntegrityViolationException;
 import com.crud.gestao.services.exceptions.ObjectnotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
+	
 	//formata os dados do retorno de obj não encontrado
 	@ExceptionHandler(ObjectnotFoundException.class)
 	public ResponseEntity<StandardError> objectnotFoundException(ObjectnotFoundException ex, 
@@ -21,4 +24,16 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
+	
+	//Retorna erro de cpf já cadastrado formatado
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, 
+			HttpServletRequest request) 	{
+		
+		StandardError	error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Violaçâo de dados", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
 }
--- src/main/java/com/crud/gestao/services/ColaboradorService.java
+++ src/main/java/com/crud/gestao/services/ColaboradorService.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.crud.gestao.domain.Colaborador;
+import com.crud.gestao.domain.Pessoa;
 import com.crud.gestao.domain.dtos.ColaboradorDTO;
 import com.crud.gestao.repositories.ColaboradorRepository;
+import com.crud.gestao.repositories.PessoaRepository;
+import com.crud.gestao.services.exceptions.DataIntegrityViolationException;
 import com.crud.gestao.services.exceptions.ObjectnotFoundException;
 
 @Service
@@ -16,7 +19,11 @@ public class ColaboradorService {
 	
 	@Autowired
 	private ColaboradorRepository repository;
-	 //procura o colaborador pelo id e da um response formatado caso não exista o id no banco
+	
+	
+	//procura o colaborador pelo id e da um response formatado caso não exista o id no banco
+	@Autowired
+	private PessoaRepository pessoarepository;
 	public Colaborador findById(Integer id) {
 		Optional<Colaborador> obj  = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id:" +id));
@@ -26,8 +33,21 @@ public class ColaboradorService {
 	}
 	public Colaborador create(ColaboradorDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfeEmail(objDTO);
 		Colaborador newObj = new Colaborador(objDTO);
 		return repository.save(newObj);
+	}
+	private void validaPorCpfeEmail(ColaboradorDTO objDTO) {
+		Optional<Pessoa> obj = pessoarepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Cpf já cadastrado no sistema");  // caso já exista o cpf
+		}
+		obj = pessoarepository.findByEmail(objDTO.getEmail());
+		
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail  já cadastrado no sistema"); // caso já exista o email
+		}
+
 	}
 
 }
