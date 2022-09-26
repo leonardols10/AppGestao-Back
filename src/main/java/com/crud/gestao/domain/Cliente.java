ddc1fef90dd1ac0d1664db7d4941d98db7265925
leonardols10 <leonardosilvalima40@gmail.com>
2022-09-25 23:24:15 -0300

    CRUD para cliente

--- src/main/java/com/crud/gestao/domain/Cliente.java
+++ src/main/java/com/crud/gestao/domain/Cliente.java
@@ -2,10 +2,12 @@ package com.crud.gestao.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.crud.gestao.domain.dtos.ClienteDTO;
 import com.crud.gestao.domain.enums.perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -23,7 +25,7 @@ public class Cliente extends Pessoa {
 	private List<Chamado> chamados  = new ArrayList<>();
 
 	public Cliente() {
-		super();
+		super();	
 		addPerfil(perfil.CLIENTE);
 	}
 
@@ -31,6 +33,17 @@ public class Cliente extends Pessoa {
 		super(id, nome, cpf, email, senha);
 		addPerfil(perfil.CLIENTE);
 
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.datacriacao = obj.getDatacriacao();
 	}
 
 	public List<Chamado> getChamados() {
