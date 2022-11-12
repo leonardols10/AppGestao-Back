46e8d695b13b485d60c77df357d3fd50b5ef281f
leonardols10 <leonardosilvalima40@gmail.com>
2022-11-12 15:47:39 -0300

    autorizacao

--- src/main/java/com/crud/gestao/config/SecurityConfig.java
+++ src/main/java/com/crud/gestao/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,9 +18,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.crud.gestao.security.JWTAuthenticationFilter;
+import com.crud.gestao.security.JWTAuthorizationFilter;
 import com.crud.gestao.security.JWTUtil;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
@@ -42,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		//como não vamos armazenar a sessao do usuario desabilitei o csrf
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, detailsService));
 		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
--- src/main/java/com/crud/gestao/resources/ColaboradorResource.java
+++ src/main/java/com/crud/gestao/resources/ColaboradorResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +50,7 @@ public class ColaboradorResource {
 	
 	
 	//Cadastra um novo colaborador
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<ColaboradorDTO> create(@Valid @RequestBody ColaboradorDTO objDTO){
 		Colaborador newObj = service.create(objDTO);
@@ -58,12 +60,15 @@ public class ColaboradorResource {
 	
 	
 	//Atualiza um colaborador
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ColaboradorDTO> update(@PathVariable Integer id, @Valid @RequestBody ColaboradorDTO objDTO ){
 		Colaborador obj = service.update(id, objDTO);
 		return ResponseEntity.ok().body(new ColaboradorDTO(obj));
 	}
 	
+	//Deleta um colaborador
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<ColaboradorDTO> delete(@PathVariable Integer id){
 			service.delete(id);
